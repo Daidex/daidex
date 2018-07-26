0aa55a5417057846357c5bad8e946e5eb43d560b
Luis Lacruz <luixlacrux18@gmail.com>
2018-07-25 22:32:33 -0500

    (DDX-22) Create link component

--- src/components/Atoms/Link/Link.js
+++ src/components/Atoms/Link/Link.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import styles from './styles.sass'
+import { get } from 'lodash'
+import classnames from 'classnames'
+
+import styles from './LinkStyles.sass'
 
 const Link = ({
-  href, children, ...rest
-}) => (
-  <a href={href} className={styles.default} {...rest}>
-    {children}
-  </a>
-);
+  href, children, className, theme, disabled, ...rest
+}) => {
+  const classNames = classnames({
+    [get(styles, theme, 'default')]: true,
+    [styles.disabled]: disabled,
+    [className]: className,
+  })
+
+  return (
+    <a
+      href={!disabled ? href : null}
+      className={classNames}
+      {...rest}
+    >
+      {children}
+    </a>
+  )
+}
 
 Link.propTypes = {
   href: PropTypes.string.isRequired,
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
+  theme: PropTypes.string,
+  disabled: PropTypes.bool,
+}
+
+Link.defaultProps = {
+  theme: 'default',
+  disabled: false,
+  className: ''
 }
 
 export default Link;
