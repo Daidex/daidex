8be1daeaaf7b5a8c5f3a8cbf749568ea66dbdaf8
luixlacrux <luixlacrux18@gmail.com>
2018-08-26 20:35:38 -0500

    Change copies

--- src/components/Organisms/MetaMaskWithError/MetaMaskWithError.js
+++ src/components/Organisms/MetaMaskWithError/MetaMaskWithError.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Row from 'src/components/Atoms/Row'
 import Link from 'src/components/Atoms/Link'
 import Text from 'src/components/Atoms/Text'
 import Button from 'src/components/Atoms/Button'
 
+import appStates from 'src/store/states/appStates'
 import metaLogo from 'assets/img/meta1.png'
 
 import styles from './MetaMaskWithErrorStyles.sass'
 import copies from './copies.json'
 
-const MetaMaskWithError = () => (
+const copyByView = {
+  [appStates.view.metaMaskIsRequired]: copies.install_meta_mask,
+  [appStates.view.metaMaskFailToConnect]: copies.unclock_meta_mask,
+  [appStates.view.metaMaskIsNotMainNet]: copies.network_not_allowed
+}
+
+const MetaMaskWithError = ({ view }) => (
   <Row className={styles.container}>
     <Row className={styles.content}>
       <Row className={styles.logo}>
@@ -19,10 +27,10 @@ const MetaMaskWithError = () => (
       <Row className={styles.texts}>
         <Text theme="h1">¡UPS!</Text>
         <Text theme="light-text" className={styles.description}>
-          {copies.default}
+          {copyByView[view] || copies.default}
         </Text>
       </Row>
-      <Row spacing={{ top: 60 }}>
+      <Row spacing={{ top: 60 }} className={styles.actions}>
         <Button
           theme="primary"
           className={styles.button}
@@ -30,14 +38,20 @@ const MetaMaskWithError = () => (
         >
           Intentar de nuevo
         </Button>
-        <Button theme="primary" className={styles.button}>
-          <Link href="https://metamask.io/" theme="inline" target="_blank">
-            Instalar
-          </Link>
-        </Button>
+        {view === appStates.view.metaMaskIsRequired && (
+          <Button theme="primary" className={styles.button}>
+            <Link href="https://metamask.io/" theme="inline" target="_blank">
+              Instalar
+            </Link>
+          </Button>
+        )}
       </Row>
     </Row>
   </Row>
 )
+
+MetaMaskWithError.propTypes = {
+  view: PropTypes.string.isRequired
+}
 
 export default MetaMaskWithError
